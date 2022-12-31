b9fef4cfba7457bb23d977fb2a74d34f93a4a2f4
tck-cy <99850439+tck-cy@users.noreply.github.com>
2022-12-30 18:25:44 -0800

    completed cipher

--- Caesars-cipher.js
+++ Caesars-cipher.js
@@ -1,1 +1,48 @@
 //starting 
+function rot13(str) {
+    let result = "";
+    let split = str.split("");
+    let lookup = {
+        A: "N",
+        B: "O",
+        C: "P",
+        D: "Q",
+        E: "R",
+        F: "S",
+        G: "T",
+        H: "U",
+        I: "V",
+        J: "W",
+        K: "X",
+        L: "Y",
+        M: "Z",
+        N: "A",
+        O: "B",
+        P: "C",
+        Q: "D",
+        R: "E",
+        S: "F",
+        T: "G",
+        U: "H",
+        V: "I",
+        W: "J",
+        X: "K",
+        Y: "L",
+        Z: "M"
+    };
+    let regex = /[,!-?. ]/gi
+    let alphabet = /[a-zA-Z]/i
+    for (let i = 0; i < split.length; i++) {
+       if (regex.test(split[i])) {
+        result += split[i];
+       }
+       if (alphabet.test(split[i])) {
+        let lookupKey = split[i];
+      result += lookup[lookupKey];
+       }
+      
+    }
+    return result;
+  }
+  
+  console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 3-Ceasars-Cipher
+just used a look up table and a pair of if statements one for punctuation and one for the letters.
